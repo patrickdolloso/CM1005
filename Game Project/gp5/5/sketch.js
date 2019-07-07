f68ac2ff5640938f1babadd2223140966f859748
Patrick Dolloso <patrick.dolloso@gmail.com>
2019-07-06 21:46:20 -0400

    commit

---
+++ Game Project/gp5/5/sketch.js
@@ -0,0 +1,171 @@
+/*
+
+The Game Project 5 - Bring it all together
+
+*/
+
+var gameChar_x;
+var gameChar_y;
+var floorPos_y;
+var scrollPos;
+var gameChar_world_x;
+
+var isLeft;
+var isRight;
+var isFalling;
+var isPlummeting;
+
+function setup()
+{
+	createCanvas(1024, 576);
+	floorPos_y = height * 3/4;
+	gameChar_x = width/2;
+	gameChar_y = floorPos_y;
+
+	// Variable to control the background scrolling.
+	scrollPos = 0;
+
+	// Variable to store the real position of the gameChar in the game
+	// world. Needed for collision detection.
+	gameChar_world_x = gameChar_x - scrollPos;
+
+	// Boolean variables to control the movement of the game character.
+	isLeft = false;
+	isRight = false;
+	isFalling = false;
+	isPlummeting = false;
+
+	// Initialise arrays of scenery objects.
+}
+
+function draw()
+{
+	background(100, 155, 255); // fill the sky blue
+
+	noStroke();
+	fill(0,155,0);
+	rect(0, floorPos_y, width, height/4); // draw some green ground
+
+	// Draw clouds.
+
+	// Draw mountains.
+
+	// Draw trees.
+
+	// Draw canyons.
+
+	// Draw collectable items.
+
+	// Draw game character.
+	
+	drawGameChar();
+
+	// Logic to make the game character move or the background scroll.
+	if(isLeft)
+	{
+		if(gameChar_x > width * 0.2)
+		{
+			gameChar_x -= 5;
+		}
+		else
+		{
+			scrollPos += 5;
+		}
+	}
+
+	if(isRight)
+	{
+		if(gameChar_x < width * 0.8)
+		{
+			gameChar_x  += 5;
+		}
+		else
+		{
+			scrollPos -= 5; // negative for moving against the background
+		}
+	}
+
+	// Logic to make the game character rise and fall.
+
+	// Update real position of gameChar for collision detection.
+	gameChar_world_x = gameChar_x - scrollPos;
+}
+
+
+// ---------------------
+// Key control functions
+// ---------------------
+
+function keyPressed(){
+
+	console.log("press" + keyCode);
+	console.log("press" + key);
+
+}
+
+function keyReleased()
+{
+
+	console.log("release" + keyCode);
+	console.log("release" + key);
+
+}
+
+
+// ------------------------------
+// Game character render function
+// ------------------------------
+
+// Function to draw the game character.
+
+function drawGameChar()
+{
+	// draw game character
+}
+
+// ---------------------------
+// Background render functions
+// ---------------------------
+
+// Function to draw cloud objects.
+
+// Function to draw mountains objects.
+
+// Function to draw trees objects.
+
+
+// ---------------------------------
+// Canyon render and check functions
+// ---------------------------------
+
+// Function to draw canyon objects.
+
+function drawCanyon(t_canyon)
+{
+
+}
+
+// Function to check character is over a canyon.
+
+function checkCanyon(t_canyon)
+{
+
+}
+
+// ----------------------------------
+// Collectable items render and check functions
+// ----------------------------------
+
+// Function to draw collectable objects.
+
+function drawCollectable(t_collectable)
+{
+
+}
+
+// Function to check character has collected an item.
+
+function checkCollectable(t_collectable)
+{
+
+}
---
+++ W12 Adv arrays and iter cont/readme.md
@@ -0,0 +1,7 @@
+# Week 12: Advanced arrays and iteration (cont.)
+* In week 12, you will start part 5 of the game project and pro stage of Sleuth. At the end of week, you will be required to submit part 4 of the game project.
+
+## Lessons
+* 6.5 Code philosophy: testing
+* 6.6 Game project 5 - bring it all together
+* 6.7 Sleuth: Start of pro stage
