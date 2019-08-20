15d21a63d36976cf7d4978fcb596b537568e059a
Patrick Dolloso <patrick.dolloso@gmail.com>
2019-08-19 21:58:14 -0400

    Week 15

---
+++ W15 Data structures and search/8.101/2dArrays/sketch.js
@@ -0,0 +1,11 @@
+var groups =[["Angus", "Ashley", "Anthony", "Anna", "Alicia"],
+            ["Benny", "Beau", "Bernie", "Betty", "Bjorn"],
+            ["Colin", "Chris", "Claire", "Catherine", "Cody"],
+            ["Daisy", "Delia", "Derek", "Donald", "Danny"],
+            ["Ernest", "Elanor", "Eliza", "Eric", "Elizabeth"],
+            ["Frida", "Freddy", "Fiona", "Finn", "Fatima"]];
+
+function setup()
+{
+  
+}
---
+++ W15 Data structures and search/readme.md
@@ -0,0 +1,1 @@
+# Week 15: Data structures and searches
